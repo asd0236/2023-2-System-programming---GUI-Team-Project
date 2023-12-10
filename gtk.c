8f51503f8e14a8500d60f0d3418744e54ac420c1
yundoun <dbsehdns12@gmail.com>
2023-12-10 17:42:33 +0900

    Feat : 메시지 발신자 표시 추
    채팅에서 각 메시지 옆에 누가 해당 메시지를 보냈는지를 표시하는 기능을 추가했습니다.

--- gtk.c
+++ gtk.c
@@ -9,56 +9,44 @@ GtkWidget *text_view, *entry;
 GtkTextBuffer *buffer;
 int client_socket;
 
+// 메시지를 전송하는 함수
 void send_message(GtkWidget *widget, gpointer data)
 {
     const gchar *message;
     GtkTextIter end;
 
-    // 텍스트 상자에서 메시지를 가져옴
-    message = gtk_entry_get_text(GTK_ENTRY(entry));
+    message = gtk_entry_get_text(GTK_ENTRY(entry));   // 입력 필드에서 메시지 가져오기
+    send(client_socket, message, strlen(message), 0); // 클라이언트 소켓을 통해 메시지 전송
 
-    // 서버에 메시지 전송
-    send(client_socket, message, strlen(message), 0);
-
-    // 텍스트 버퍼의 끝 반복자를 가져옴
     gtk_text_buffer_get_end_iter(buffer, &end);
-
-    // 보낸 메시지를 텍스트 버퍼에 추가
-    gtk_text_buffer_insert(buffer, &end, message, -1);
-    gtk_text_buffer_insert(buffer, &end, "\n", -1);
-
-    // 메시지 전송 후 텍스트 상자 초기화
-    gtk_entry_set_text(GTK_ENTRY(entry), "");
+    gtk_text_buffer_insert(buffer, &end, message, -1); // 채팅 창에 메시지 추가
+    gtk_text_buffer_insert(buffer, &end, "\n", -1);    // 새로운 줄 추가
+    gtk_entry_set_text(GTK_ENTRY(entry), "");          // 입력 필드 비우기
 }
 
+// 메시지를 채팅 창에 업데이트하는 함수
 void update_messages(const char *message)
 {
     GtkTextIter end;
 
-    // 텍스트 버퍼의 끝 반복자를 가져옴
     gtk_text_buffer_get_end_iter(buffer, &end);
-
-    // 받은 메시지를 텍스트 버퍼에 추가
-    gtk_text_buffer_insert(buffer, &end, message, -1);
-    gtk_text_buffer_insert(buffer, &end, "\n", -1);
+    gtk_text_buffer_insert(buffer, &end, message, -1); // 채팅 창에 메시지 추가
+    gtk_text_buffer_insert(buffer, &end, "\n", -1);    // 새로운 줄 추가
 }
 
+// 메시지 수신을 처리하는 스레드 함수
 void *receive_messages(void *arg)
 {
     char message[1024];
     ssize_t recv_size;
 
-    // 서버로부터 메시지 수신
     while ((recv_size = recv(client_socket, message, sizeof(message), 0)) > 0)
     {
         message[recv_size] = '\0';
-        update_messages(message);
+        gdk_threads_add_idle((GSourceFunc)update_messages, strdup(message)); // 메시지를 채팅 창에 업데이트
     }
 
-    // 서버와의 연결이 닫힘
-    update_messages("서버 연결이 종료되었습니다.\n");
-
-    // 스레드 종료
+    update_messages("서버와 연결이 끊어졌습니다.\n"); // 서버와 연결이 끊겼을 때 메시지 표시
     pthread_exit(NULL);
 }
 
@@ -70,40 +58,32 @@ int main(int argc, char *argv[])
 
     gtk_init(&argc, &argv);
 
-    // 메인 창 생성
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Chatting Client");
+    gtk_window_set_title(GTK_WINDOW(window), "채팅 클라이언트");
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
     gtk_widget_set_size_request(window, 300, 200);
 
-    // 수직 상자 생성
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), box);
 
-    // 텍스트 뷰 (읽기 전용)와 스크롤 윈도우 생성
     text_view = gtk_text_view_new();
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE); // 읽기 전용 설정
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
     buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
     scroll = gtk_scrolled_window_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER(scroll), text_view);
     gtk_box_pack_start(GTK_BOX(box), scroll, TRUE, TRUE, 0);
 
-    // 텍스트 상자 생성
     entry = gtk_entry_new();
     gtk_box_pack_start(GTK_BOX(box), entry, FALSE, FALSE, 0);
 
-    // Send 버튼 생성
-    button = gtk_button_new_with_label("Send");
+    button = gtk_button_new_with_label("전송");
     g_signal_connect(button, "clicked", G_CALLBACK(send_message), NULL);
     gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
 
-    // 윈도우 닫기 이벤트 설정
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // 창에 있는 모든 위젯 표시
     gtk_widget_show_all(window);
 
-    // 서버와의 연결 설정
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1)
     {
@@ -111,38 +91,30 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // 서버 주소 설정
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 서버의 IP 주소
+    server_addr.sin_port = htons(8080);                   // 서버의 포트 번호
 
-    // 서버에 연결
     if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
-        perror("서버 연결 오류");
+        perror("연결 오류");
         exit(EXIT_FAILURE);
     }
 
-    // 사용자로부터 사용자 이름 입력
     char username[50];
     printf("사용자 이름을 입력하세요: ");
     fgets(username, sizeof(username), stdin);
     username[strcspn(username, "\n")] = '\0';
+    send(client_socket, username, sizeof(username), 0); // 사용자 이름을 서버에 전송
 
-    // 사용자 이름을 서버에 전송
-    send(client_socket, username, sizeof(username), 0);
-
-    // 메시지 수신을 처리하는 스레드 생성
     if (pthread_create(&recv_thread, NULL, receive_messages, NULL) != 0)
     {
-        perror("메시지 수신 스레드 생성 오류");
+        perror("스레드 생성 오류");
         exit(EXIT_FAILURE);
     }
 
-    // GTK 메인 루프 실행
     gtk_main();
 
-    // 클라이언트 소켓 및 스레드 정리
     close(client_socket);
     pthread_join(recv_thread, NULL);
 
